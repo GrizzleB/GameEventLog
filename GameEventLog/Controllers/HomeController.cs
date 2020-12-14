39947b8fab072de752af6bdd0cfbb80dd2c44ff1
Ryan Gerhardt <rgerhardt2@my.stlcc.edu>
2020-12-14 00:24:24 -0600

    Index updated

--- GameEventLog/Controllers/HomeController.cs
+++ GameEventLog/Controllers/HomeController.cs
@@ -6,27 +6,38 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameEventLog.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        //private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        //
+        private EventContext context { get; set; }
+        public HomeController(EventContext ctx)
         {
-            _logger = logger;
+            context = ctx;
         }
-
         public IActionResult Index()
         {
-            return View();
+            var events = context.Events;           
+            
+            return View(events);
         }
 
         public IActionResult Privacy()
         {
             return View();
         }
+        //       
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
